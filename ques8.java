e36caa7da7093354175e44f589db89f8464df6f3
DeeptiGarg22 <106006685+DeeptiGarg22@users.noreply.github.com>
2022-09-14 01:21:40 +0530

    Assignment 2 Question 8(Level 1)

--- ques8.java
+++ ques8.java
@@ -1,22 +1,21 @@
-package assignment1;
+package assignment2;
 import java.util.*;
 public class ques8 {
-    public static void main(String args[]){
-        String name,sec,course;
-        long roll;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the name of Student");
-        name=sc.nextLine();
-        System.out.println("Enter teh section of Student");
-        sec=sc.nextLine();
-        System.out.println("Enter the course of Student");
-        course=sc.nextLine();
-        System.out.println("Enter the roll no. of Student");
-        roll=sc.nextLong();
-        System.out.println("The Student Data is:");
-        System.out.println(name);
-        System.out.println(sec);
-        System.out.println(course);
-        System.out.println(roll);
-    }
-}
+        public static void main(String args[]){
+            int ch,ca;
+            float atd;
+            Scanner sc=new Scanner (System.in);
+            System.out.println("The Classes held");
+            ch=sc.nextInt();
+            System.out.println("The Classes Attended");
+            ca=sc.nextInt();
+            atd=(ca/ch)*100;
+            if(atd>75.0){
+                System.out.println("Allowed for Exam");
+            }
+            else{
+                System.out.println("Not Allowed");
+            }
+        }
+    }   
+
