6a8b667107fdf1667e22f6290cc96a910a919376
DeeptiGarg22 <106006685+DeeptiGarg22@users.noreply.github.com>
2022-09-14 00:43:55 +0530

    Assignment 1 Question 2

---
+++ ques2.java
@@ -0,0 +1,14 @@
+package assignment1;
+import java.util.*;
+public class ques2 {
+    public static void main(String args[]){
+        int a,b,c=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 2 no.");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=a+b;
+        System.out.println("The sum of 2 no.s is:"+c);
+    }
+}
+
