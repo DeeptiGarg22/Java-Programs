1e19360ef5ae4ab2e53bd15a57bd33ff790e3780
DeeptiGarg22 <106006685+DeeptiGarg22@users.noreply.github.com>
2022-09-14 01:20:26 +0530

    Assignment 2 Question 5(Level 1)

--- ques5.java
+++ ques5.java
@@ -1,13 +1,28 @@
-package assignment1;
+package assignment2;
 import java.util.*;
 public class ques5 {
     public static void main(String args[]){
-        int side,area=0;
+        int marks;
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the side of square");
-        side=sc.nextInt();
-        area=side*side;
-        System.out.println("The Area of Square is: "+area);
+        System.out.println("Enter the marks of Student");
+        marks=sc.nextInt();
+        if(marks>80){
+            System.out.println("The Grade is: A");
+        }
+        if(marks>60&&marks<80){
+            System.out.println("The Grade is: B");
+        }
+        if(marks>50&&marks<60){
+            System.out.println("The Grade is: C");
+        }
+        if(marks>45&&marks<50){
+            System.out.println("The Grade is: D");
+        }
+        if(marks>25&&marks<45){
+            System.out.println("The Grade is: E");
+        }
+        if(marks<25){
+            System.out.println("The Grade is: F");
+        }
     }
 }
-
