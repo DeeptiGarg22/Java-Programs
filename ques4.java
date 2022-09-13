c8190f1985b8d8b971002310d5a11522a8d2d1cf
DeeptiGarg22 <106006685+DeeptiGarg22@users.noreply.github.com>
2022-09-14 01:20:03 +0530

    Assignment 2 Question 4(Level 1)

--- ques4.java
+++ ques4.java
@@ -1,13 +1,20 @@
-package assignment1;
+package assignment2;
 import java.util.*;
 public class ques4 {
     public static void main(String args[]){
-        double r,area=0.0;
+        int ser;
+        double sal,bonus=0.0;
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the radius of circle");
-        r=sc.nextDouble();
-        area=3.14*(r*r);
-        System.out.println("The Area of Circle is:"+area);
+        System.out.println("Enter the Service Year of Employee");
+        ser=sc.nextInt();
+        System.out.println("Enter the Salary of Employee");
+        sal=sc.nextDouble();
+        if(ser>5){
+            bonus=sal+(sal*5)/100;
+            System.out.println("The net Amount of the Employee is: "+bonus);
+        }
+        else{
+            System.out.println("The net Amount of the Employee is: "+sal);
+        }
     }
 }
-
