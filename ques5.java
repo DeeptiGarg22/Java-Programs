8cb314bee923d93c51047357336ddb2e03962f80
DeeptiGarg22 <106006685+DeeptiGarg22@users.noreply.github.com>
2022-09-14 00:45:02 +0530

    Assignment 1 Question 5

---
+++ ques5.java
@@ -0,0 +1,13 @@
+package assignment1;
+import java.util.*;
+public class ques5 {
+    public static void main(String args[]){
+        int side,area=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the side of square");
+        side=sc.nextInt();
+        area=side*side;
+        System.out.println("The Area of Square is: "+area);
+    }
+}
+
