aacfa41de166630c55deff9586b6bd5d4bae05ea
DeeptiGarg22 <106006685+DeeptiGarg22@users.noreply.github.com>
2022-09-14 01:20:57 +0530

    Assignment 2 Question 6(Level 1)

--- ques6.java
+++ ques6.java
@@ -1,15 +1,24 @@
-package assignment1;
+package assignment2;
 import java.util.*;
 public class ques6 {
     public static void main(String args[]){
-        int a,b,c,y;
+        int age1,age2,age3;
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the no.");
-        a=sc.nextInt();
-        b=sc.nextInt();
-        c=sc.nextInt();
-        y=a>b&&a>c?a:b>c?b:c;
-        System.out.println("The Greatest Number is: "+y);
+        System.out.println("Enter the age of 3 people");
+        age1=sc.nextInt();
+        age2=sc.nextInt();
+        age3=sc.nextInt();
+        if(age1>age2&&age1>age3){
+            System.out.println("The Oldest among them is: "+age1);
+        }
+        if(age2>age1&&age2>age3){
+            System.out.println("The Oldest among them is: "+age2);
+        }
+        if(age3>age1&&age3>age2){
+            System.out.println("The Oldest among them is: "+age3);
+        }
+        else{
+            System.out.println("The have Equal Ages");
+        }
     }
-
 }
